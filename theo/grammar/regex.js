764ee3eb0841541c5c7062f99f8b0b7d4dc92d47
ModellbahnFreak <freak.mdellbahn@gmail.com>
2020-02-22 20:45:56 +0100

    Added automata to regex

--- theo/grammar/regex.js
+++ theo/grammar/regex.js
@@ -46,15 +46,13 @@ function regexCreateWords() {
     alphabet.push("");
     gui.out.value = "Fond words: \n";
     var foundWords = [];
-    for (var i = 0; i < 10000; i++) {
-        var w = genRndWord(alphabet, maxLen);
+    var words = genAllWords(alphabet, maxLen);
+    words.forEach(w => {
         if (w.match(regex) && foundWords.indexOf(w) < 0) {
             gui.out.value += w + ", ";
             foundWords.push(w);
-        } else {
-            console.log(w);
         }
-    }
+    });
 }
 
 function genRndWord(alphabet, len) {
@@ -63,4 +61,117 @@ function genRndWord(alphabet, len) {
         word += alphabet[Math.floor(Math.random() * alphabet.length)];
     }
     return word;
+}
+
+function genAllWords(alphabet, length) {
+    if (length <= 0) {
+        return [""];
+    }
+    var allWordsShorter = [""];//genAllWords(alphabet, length - 1);
+    var lastStepWords = allWordsShorter;
+    for (var i = 1; i <= length; i++) {
+        var newWords = [];
+        lastStepWords.forEach(w => {
+            alphabet.forEach(x => {
+                newWords.push(w + x);
+            });
+        });
+        allWordsShorter = allWordsShorter.concat(newWords);
+        lastStepWords = newWords;
+    }
+    return allWordsShorter;
+}
+
+function automataToRegex() {
+    if (automParse.type != "DFA") {
+        gui.out.value = "Please input a dfa or convert the automata to a deterministic one.";
+        return;
+    }
+    var r = [];
+    var n = automParse.states.length;
+    var k = 0;
+    var states = [];
+    for (var i = 1; i <= n; i++) {
+        states[i] = automParse.states[i - 1];
+    }
+    r[k] = [];
+    for (var i = 1; i <= n; i++) {
+        r[k][i] = [];
+        for (var j = 1; j <= n; j++) {
+            r[k][i][j] = "";
+            var numAdded = 0;
+            var words = genAllWords(automParse.alphabet, n);
+            words.forEach(w => {
+                var z = i;
+                var graterK = false;
+                for (var a = 0; a < w.length; a++) {
+                    z = states.indexOf(automParse.function[states[z]][w.charAt(a)]);
+                    if (z == -1) {
+                        break;
+                    }
+                    if (a < w.length - 1 && z > k) {
+                        graterK = true;
+                    }
+                }
+                if (!graterK && z == j) {
+                    r[k][i][j] += w + "|";
+                    numAdded++;
+                }
+            });
+            if (r[k][i][j] == "|" && numAdded == 1) {
+                r[k][i][j] = "";
+            } else if (r[k][i][j] == "" && numAdded == 0) {
+                r[k][i][j] = null;
+            } else {
+                if (numAdded == 1) {
+                    r[k][i][j] = r[k][i][j].substr(0, r[k][i][j].length - 1);
+                } else {
+                    r[k][i][j] = "(" + r[k][i][j].substr(0, r[k][i][j].length - 1) + ")";
+                }
+            }
+        }
+    }
+    for (k = 1; k <= n; k++) {
+        r[k] = [];
+        for (var i = 1; i <= n; i++) {
+            r[k][i] = [];
+            for (var j = 1; j <= n; j++) {
+                if (r[k - 1][i][j] == null) {
+                    if (r[k - 1][i][k] == null || r[k - 1][k][k] == null || r[k - 1][k][j] == null) {
+                        r[k][i][j] = null;
+                    } else {
+                        if (r[k - 1][i][k] == "" && r[k - 1][k][k] == "" && r[k - 1][k][j] == "") {
+                            r[k - 1][i][k] = "";
+                        } else {
+                            if (r[k - 1][k][k] == "") {
+                                r[k][i][j] = "(" + r[k - 1][i][k] + r[k - 1][k][j] + ")";
+                            } else {
+                                r[k][i][j] = "(" + r[k - 1][i][k] + "(" + r[k - 1][k][k] + ")*" + r[k - 1][k][j] + ")";
+                            }
+                        }
+                    }
+                } else {
+                    if (r[k - 1][i][k] == null || r[k - 1][k][k] == null || r[k - 1][k][j] == null) {
+                        r[k][i][j] = r[k - 1][i][j];
+                    } else {
+                        if (r[k - 1][i][k] == "" && r[k - 1][k][k] == "" && r[k - 1][k][j] == "") {
+                            r[k][i][j] = r[k - 1][i][j];
+                        } else {
+                            if (r[k - 1][k][k] == "") {
+                                r[k][i][j] = "(" + r[k - 1][i][j] + "|" + r[k - 1][i][k] + r[k - 1][k][j] + ")";
+                            } else {
+                                r[k][i][j] = "(" + r[k - 1][i][j] + "|" + r[k - 1][i][k] + "(" + r[k - 1][k][k] + ")*" + r[k - 1][k][j] + ")";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    console.log(r);
+    var erg = "";
+    for (var i = 0; i < automParse.accept.length; i++) {
+        erg += r[n][states.indexOf(automParse.start)][states.indexOf(automParse.accept[i])] + "|";
+    }
+    gui.out.value = "(" + erg.substr(0, erg.length - 1) + ")";
 }
